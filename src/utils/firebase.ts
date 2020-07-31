d46ba224ba605cc903e68e3cf29564dabe90cc59
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-07-31 09:29:10 +0300

    fix: firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 import { customAlphabet } from 'nanoid/async'
 
 export const credentials = {
-  apiKey: String(process.env.FIREBASE_API_KEY),
-  authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
-  databaseURL: String(process.env.FIREBASE_DATABASE_URL),
-  projectId: String(process.env.FIREBASE_PROJECT_ID),
-  storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET),
-  messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
-  appId: String(process.env.FIREBASE_APP_ID),
-  measurementId: String(process.env.FIREBASE_MEASUREMENT_ID)
+  apiKey: 'AIzaSyBcXZcO0o2nrmCUChr55gMS4_SbmvDprWc',
+  authDomain: 'lopsi-ef5b7.firebaseapp.com',
+  databaseURL: 'https://lopsi-ef5b7.firebaseio.com',
+  projectId: 'lopsi-ef5b7',
+  storageBucket: 'lopsi-ef5b7.appspot.com',
+  messagingSenderId: '566541013025',
+  appId: '1:566541013025:web:635f76ed0859d7501dd360',
+  measurementId: 'G-8JCGQPRQ2B'
 }
 
 export const init = (): void => {
