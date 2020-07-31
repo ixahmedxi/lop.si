c9d661f38eeb3b8c8287daf9a8f2634bd8f437f8
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-07-31 09:16:37 +0300

    modifies firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 import { customAlphabet } from 'nanoid/async'
 
 export const credentials = {
-  apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
-  authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
-  databaseURL: String(process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL),
-  projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
-  storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
-  messagingSenderId: String(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-  appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
-  measurementId: String(process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID)
+  apiKey: String(process.env.FIREBASE_API_KEY),
+  authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
+  databaseURL: String(process.env.FIREBASE_DATABASE_URL),
+  projectId: String(process.env.FIREBASE_PROJECT_ID),
+  storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(process.env.FIREBASE_APP_ID),
+  measurementId: String(process.env.FIREBASE_MEASUREMENT_ID)
 }
 
 export const init = (): void => {
