c8cc8b2a4c919e141610c36b9e3784f9814d26c3
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-08-02 19:57:17 +0300

    fix: safari mobile 100vh taking more than expected

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,19 @@ import { GlobalStyles } from '@shared/GlobalStyles'
 import { NavBar } from '@shared/NavBar'
 import { init } from '@utils/firebase'
 import { AppProps } from 'next/app'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import { jsx, ThemeProvider } from 'theme-ui'
 import { theme } from '../theme'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const [appHeight, setAppheight] = useState('100vh')
   useEffect(() => {
     init()
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', () => setAppheight(String(window.innerHeight) + 'px'))
+    }
   })
   return (
     <ThemeProvider theme={theme}>
@@ -25,8 +30,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
             '60px auto 75px',
             '100px auto 100px'
           ],
-          height: '100vh',
-          maxHeight: '100vh'
+          height: appHeight
         }}
       >
         <NavBar />
