e9f4fdcc184c7f63315a35f69d19c15e0d1d708d
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-09-23 07:43:40 +0100

    Feature: updates privacy policy

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -74,6 +74,17 @@ const PrivacyPolicy: React.FC = () => {
         <li>Find and prevent fraud</li>
       </ul>
 
+      <h3>Links to other websites</h3>
+
+      <p>
+        Our website may contain links to other websites of interest. However, once you have used
+        these links to leave our site, you should note that we do not have any control over that
+        other website. Therefore, we cannot be responsible for the protection and privacy of any
+        information which you provide whilst visiting such sites and such sites are not governed by
+        this privacy statement. You should exercise caution and look at the privacy statement
+        applicable to the website in question.
+      </p>
+
       <h3>Log Files</h3>
 
       <p>
